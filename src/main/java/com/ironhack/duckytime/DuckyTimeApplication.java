92f065e70eaa4c49b6c0451f26d556beae613ec9
Amber Yeh <266454930+amberyeh41@users.noreply.github.com>
2026-06-03 11:29:37 +0200

    first commit

---
+++ src/main/java/com/ironhack/duckytime/DuckyTimeApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.duckytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckyTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuckyTimeApplication.class, args);
+    }
+
+}
