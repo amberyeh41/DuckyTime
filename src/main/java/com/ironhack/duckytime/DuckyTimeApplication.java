a22b266b77f626e309bd2f12a3af5f467ab0e4c0
Amber Yeh <266454930+amberyeh41@users.noreply.github.com>
2026-06-03 12:16:22 +0200

    Set up MySQL and Admin model

--- src/main/java/com/ironhack/duckytime/DuckyTimeApplication.java
+++ src/main/java/com/ironhack/duckytime/DuckyTimeApplication.java
@@ -1,7 +1,11 @@
 package com.ironhack.duckytime;
 
+import com.ironhack.duckytime.models.Admin;
+import com.ironhack.duckytime.repositories.AdminRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DuckyTimeApplication {
@@ -10,4 +14,15 @@ public class DuckyTimeApplication {
         SpringApplication.run(DuckyTimeApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner run(
+            AdminRepository adminRepo
+    ){
+        return  args -> {
+            Admin alex = adminRepo.save(
+                    new Admin("alex", "alex123")
+            );
+            adminRepo.save(alex);
+        };
+    }
 }
---
+++ src/main/java/com/ironhack/duckytime/repositories/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.ironhack.duckytime.repositories;
+
+import com.ironhack.duckytime.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+}
